e8abc2763941d42b692212e1ca387f342f55b80e
farabi1 <rashidfarabi@gmail.com>
2026-02-17 15:50:56 +0600

    Funtionalities  Setup

---
+++ category.js
@@ -0,0 +1,4 @@
+const categoryContainer = document.getElementById("categoryContainer");
+const productContainer = document.getElementById("productContainer");
+const modal = document.getElementById("modal");
+const modalContent = document.getElementById("modalContent");
