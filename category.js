84a8e4fb1d731dc844ffb4b29f9b4c135ee2da2a
farabi1 <rashidfarabi@gmail.com>
2026-02-17 15:53:24 +0600

    API Setup

--- category.js
+++ category.js
@@ -2,3 +2,9 @@ const categoryContainer = document.getElementById("categoryContainer");
 const productContainer = document.getElementById("productContainer");
 const modal = document.getElementById("modal");
 const modalContent = document.getElementById("modalContent");
+
+async function loadCategories() {
+
+    const res = await fetch("https://fakestoreapi.com/products/categories");
+    const categories = await res.json();
+}
